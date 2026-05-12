76440a3a37f2a80dde7e46a375a1946040179e1f
Lingceng Ma <lingcengma@mednet.ucla.edu>
2026-05-12 13:05:27 -0700

    plot GT time series and fix sampling issue

--- utilities/sampling/encodeOneNonCartesianReadout.m
+++ utilities/sampling/encodeOneNonCartesianReadout.m
@@ -14,7 +14,7 @@ currSl = z(1);
 kloc2 = kloc;
 kloc2(3,:,:) = 0;
 obj = nufft_3d(kloc2,[app.matrixAP app.matrixRL 1],'radial',1,'gpu',0);
-slIdx = currSl + app.matrixFH/2 + 1;
+slIdx = currSl + app.matrixFH/2 ;%+ 1;
 kline = obj.fNUFT(squeeze(tmp2(:,:,slIdx)));
 kline = reshape(kline, [], 1);
 
